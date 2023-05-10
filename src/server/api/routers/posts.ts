4f3d386c2c17d48424b620ccd33df52b04b462d2
eldarcodes <prog.eldar@gmail.com>
2023-05-10 20:37:38 +0300

    feat(posts): add create post trpc route

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -4,7 +4,11 @@ import { z } from "zod";
 
 import type { User } from "@clerk/nextjs/server";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -48,4 +52,23 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.currentUser.id;
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
