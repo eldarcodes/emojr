ea30f56d3cb6601d285da56542b01ca8169faf11
eldarcodes <prog.eldar@gmail.com>
2023-05-17 18:24:19 +0300

    feat(post): add single post view page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,17 +1,55 @@
 import Head from "next/head";
 
-import type { NextPage } from "next";
+import { PageLayout } from "~/components/PageLayout";
+import { PostView } from "~/components/PostView";
+import { generateSSGHelper } from "~/server/helpers/ssg";
+import { api } from "~/utils/api";
 
-const PostPage: NextPage = () => {
+import type { GetStaticProps, NextPage } from "next";
+
+const PostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({ id });
+
+  if (!data) return <div>404</div>;
+
+  const pageTitle = data.post.content
+    ? `${data.post.content} | Emojr`
+    : "Emojr";
+
   return (
     <>
       <Head>
-        <title>Post | Emojr</title>
+        <title>{pageTitle}</title>
       </Head>
 
-      <main className="flex h-screen justify-center">Post view</main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
 };
 
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const id = context.params?.id;
+
+  if (typeof id !== "string") throw new Error("Invalid post id");
+
+  await ssg.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
+};
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
 export default PostPage;
